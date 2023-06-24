1a9f1c48dc991d3330eedb5ec5ec903606eb8225
SnoopyCodeX <extremeclasherph@gmail.com>
2023-06-24 14:31:02 +0800

    Updated AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +23,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+
+        // If app is not in debug mode and not in local environment, force url scheme to 'https'
+        // echo "Debug: " . (env('APP_DEBUG') ? 'true' : 'false') . "<br>Env: " . env('APP_ENV');
+        if(!env('APP_DEBUG', true) && env('APP_ENV') != 'local')
+            URL::forceScheme('https');
     }
 }
