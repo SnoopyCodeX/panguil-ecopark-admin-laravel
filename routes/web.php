c9f43c17036bf4a15a2084cb7daf6de98c2a3449
SnoopyCodeX <extremeclasherph@gmail.com>
2023-06-29 15:02:00 +0800

    Refactored routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\AddTouristsController;
+use App\Http\Controllers\AssignTourGuidesController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MapTrackingController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegisteredTouristsController;
+use App\Http\Controllers\ReservationsController;
+use App\Http\Controllers\TourGuidesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,5 +32,17 @@ Route::middleware('auth')->prefix('admin')->group(function() {
     Route::get('/', fn () => redirect('/admin/dashboard'));
 
     Route::get('/account/logout', [AuthController::class, 'logout'])->name('admin.logout');
-    Route::get('/{page}', [AdminController::class, 'showPage']);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::get('/reservations', [ReservationsController::class, 'index'])->name('admin.reservations');
+
+    Route::get('/registered-tourists', [RegisteredTouristsController::class, 'index'])->name('admin.registered-tourists');
+    Route::get('/add-tourist', [AddTouristsController::class, 'index'])->name('admin.add-tourist');
+
+    Route::get('/tour-guides', [TourGuidesController::class, 'index'])->name('admin.tour-guides');
+    Route::get('/assign-tour-guide', [AssignTourGuidesController::class, 'index'])->name('admin.assign-tour-guide');
+
+    Route::get('/tracking', [MapTrackingController::class, 'index'])->name('admin.map-tracking');
+
+    Route::get('/profile', [ProfileController::class, 'index'])->name('admin.profile');
+    Route::put('/profile/update', [ProfileController::class, 'updateProfile'])->name('admin.profile.update');
 });
