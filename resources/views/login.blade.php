4e239d2b48994679b3566201ec7d8ed1cc42bbbd
SnoopyCodeX <extremeclasherph@gmail.com>
2023-06-30 20:03:07 +0800

    Added type attribute in favicon meta tag

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -34,7 +34,7 @@
     <link rel="stylesheet" href="{{ asset('assets/css/demo2/style.css') }}">
     <!-- End layout styles -->
 
-    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/png" />
 </head>
 
 <body>
