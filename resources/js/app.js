2d043b5bc25e4623e008fa7e65446b8c18551c92
SnoopyCodeX <extremeclasherph@gmail.com>
2023-07-03 15:44:42 +0800

    Implemented live user/tourist map tracking

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,50 @@
+import Pusher from 'pusher-js';
 import './bootstrap';
 
 document.addEventListener('DOMContentLoaded', function() {
+    let userLocations = JSON.parse(window.localStorage.getItem('user-locations')) ?? {};
     let url = window.location.href;
+    let markers = {};
+    let map = undefined;
+
+    let pusher = new Pusher(`${import.meta.env.VITE_PUSHER_APP_KEY}`, {
+        cluster: `${import.meta.env.VITE_PUSHER_APP_CLUSTER}`
+    });
+
+    let updateMap = () => {
+        for(let id in userLocations) {
+            let userLocationData = userLocations[id];
+            let { latitude, longitude, name } = userLocationData;
+
+            if(markers[id] === undefined) {
+                markers[id] = L.marker([latitude, longitude], {title: name}).addTo(map);
+                markers[id].bindPopup(name);
+            } else
+                markers[id].setLatLng([latitude, longitude]).update();
+        }
+    };
+
+    let updateUserLocationsInLS = (newUserLocations) => window.localStorage.setItem('user-locations', JSON.stringify(newUserLocations));
+
+    let mapChannel = pusher.subscribe('map-channel');
+    mapChannel.bind('update-user-location', (data) => {
+        userLocations[data.id] = {...data.data};
+        updateUserLocationsInLS(userLocations);
+
+        if(url.endsWith('tracking')) {
+            let { latitude, longitude, name } = data.data;
+
+            // Create/Update marker on the map
+            if(markers[data.id] === undefined) {
+                markers[data.id] = L.marker([latitude, longitude], {title: name}).addTo(map);
+                markers[data.id].bindPopup(name);
+            } else
+                markers[data.id].setLatLng([latitude, longitude]).update();
+        }
+    });
 
     if(url.endsWith('tracking')) {
-        let map = L.map('map').setView([14.413447, 121.480293], 18);
+        map = L.map('map').setView([14.413447, 121.480293], 18);
 
         let tile = L.tileLayer(`https://api.mapbox.com/styles/v1/snoopycodex/clir72wpr00ko01r854fsammn/tiles/256/{z}/{x}/{y}@2x?access_token=${import.meta.env.VITE_MAPBOX_ACCESS_TOKEN}`, {
             maxZoom: 19,
@@ -19,6 +59,8 @@ document.addEventListener('DOMContentLoaded', function() {
             weight: 1,
             radius: 60
         }).addTo(map);
+
+        updateMap();
     }
 
     if(url.endsWith('assign-tour-guide')) {
