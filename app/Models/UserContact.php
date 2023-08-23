5685978f085ea8fb476ddc3eafd82208bd0bc77a
SnoopyCodeX <extremeclasherph@gmail.com>
2023-08-24 01:47:19 +0800

    Created new UserContact model with Many-to-One relationship with User model

---
+++ app/Models/UserContact.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserContact extends Model
+{
+    use HasFactory;
+
+    /**
+     * Get the owner of this contact
+     */
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'contact_name',
+        'contact_role',
+        'contact_number',
+    ];
+
+}
