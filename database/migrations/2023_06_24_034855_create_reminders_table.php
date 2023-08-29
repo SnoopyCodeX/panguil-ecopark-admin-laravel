e56960aa73d3250a1fe42450c0123a95c381d0de
SnoopyCodeX <extremeclasherph@gmail.com>
2023-08-30 02:29:53 +0800

    Added user_id column to reminders table

--- database/migrations/2023_06_24_034855_create_reminders_table.php
+++ database/migrations/2023_06_24_034855_create_reminders_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('reminders', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('user_id');
             $table->string('content', 255);
             $table->string('profile');
             $table->timestamps();
