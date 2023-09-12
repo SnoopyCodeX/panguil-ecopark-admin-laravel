cc652986293c067d1b13aeb3372df77331e243e9
SnoopyCodeX <extremeclasherph@gmail.com>
2023-09-12 17:47:32 +0800

    Updated tracking.blade.php

--- resources/views/admin/tracking.blade.php
+++ resources/views/admin/tracking.blade.php
@@ -6,6 +6,9 @@
 
 @section('tracking-scripts')
 <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+<script>
+    var user = {!! auth()->user()->toJson() !!};
+</script>
 @endsection
 
 @section('tracking')
