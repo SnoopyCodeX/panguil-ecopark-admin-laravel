718602e217aa525ac2736aeed47eade7e5f9f38b
SnoopyCodeX <extremeclasherph@gmail.com>
2023-09-15 04:49:52 +0800

    Added php doc for better linting and code suggestion

--- app/Models/Geofence.php
+++ app/Models/Geofence.php
@@ -5,6 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Geofence
+ *
+ * @method static \Database\Factories\GeofenceFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Geofence newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Geofence newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Geofence query()
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Geofence extends Model
 {
     use HasFactory;
