481e5e2ebba81a8a6b05294876f379987ab1fa1c
SnoopyCodeX <extremeclasherph@gmail.com>
2023-09-15 21:59:22 +0800

    Added api routes for geofence actions

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\GeofenceController;
 use App\Http\Controllers\api\UserController;
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +19,17 @@ use Illuminate\Support\Facades\Route;
 Route::post('login', [AuthController::class, 'apiLogin']);
 Route::post('register', [AuthController::class, 'apiRegister']);
 Route::get('check-authentication', [AuthController::class, 'apiCheckAuthentication']);
+
+Route::prefix('system-admin')->group(function() {
+    Route::get('geofences', [GeofenceController::class, 'getGeofences'])->name('system-admin.geofences');
+    Route::post('geofences/save', [GeofenceController::class, 'saveGeofence'])->name('system-admin.geofences.save');
+    Route::post('geofences/{id}/update', [GeofenceController::class, 'updateGeofence'])->name('system-admin.geofences.update');
+    Route::post('geofences/{id}/delete', [GeofenceController::class, 'deleteGeofence'])->name('system-admin.geofences.delete');
+});
+
+Route::middleware(['jwt.auth', 'jwt.not_revoked'])->prefix('system-client')->group(function() {
+    Route::get('geofences', [GeofenceController::class, 'getGeofences']);
+});
 
 Route::middleware(['jwt.auth', 'jwt.not_revoked'])->prefix('user')->group(function() {
     Route::post('logout', [AuthController::class, 'apiLogout']);
