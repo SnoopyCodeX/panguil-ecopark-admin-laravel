683cf9c2abed10e4530b772e3e261cd3342d1943
SnoopyCodeX <extremeclasherph@gmail.com>
2023-09-19 09:28:00 +0800

    Added ide docs in Geofence model

--- app/Models/Geofence.php
+++ app/Models/Geofence.php
@@ -13,6 +13,15 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|Geofence newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Geofence query()
  * @mixin \Illuminate\Database\Eloquent\Builder
+ * @property int $id
+ * @property string $geojson
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Geofence whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Geofence whereGeojson($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Geofence whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Geofence whereUpdatedAt($value)
+ * @mixin \Illuminate\Database\Eloquent\Builder
  */
 class Geofence extends Model
 {
