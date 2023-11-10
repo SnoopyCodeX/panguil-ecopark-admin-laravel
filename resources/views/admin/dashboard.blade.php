c16bf2597bc7267ec7cd757b9510b529d446e1de
SnoopyCodeX <extremeclasherph@gmail.com>
2023-11-10 21:03:28 +0800

    Added registered tourists table to the dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,6 +3,7 @@
 @section('dashboard-css')
     <!-- Plugin css for this page -->
     <link rel="stylesheet" href="{{ asset('assets/vendors/flatpickr/flatpickr.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css') }}">
     <!-- End plugin css for this page -->
 @endsection
 
@@ -11,7 +12,32 @@
     <script src="{{ asset('assets/vendors/flatpickr/flatpickr.min.js') }}"></script>
     <script src="{{ asset('assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
     <script src="{{ asset('assets/vendors/moment/moment.min.js') }}"></script>
+
+    <script src="{{ asset('assets/vendors/datatables.net/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('assets/vendors/datatables.net-bs5/dataTables.bootstrap5.js') }}"></script>
     <!-- End plugin js for this page -->
+
+    <script>
+        $(document).on("DOMContentLoaded", function() {
+            $("#registeredTouristsTable").DataTable({
+                serverSide: true,
+                ajax: "{{ route('admin.registered-tourists') }}",
+                columns: [
+                    {data: 'id', name: 'id'},
+                    {data: 'name', name: 'name'},
+                    {data: 'gender', name: 'gender', render: (data) => data.charAt(0).toUpperCase() + data.slice(1)},
+                    {data: 'age', name: 'age', render: (data) => `${data} yrs. old`},
+                    {data: 'cellphone_number', name: 'cellphone_number', render: (data) => data ?? 'N/A'},
+                    {data: 'assign_datetime', name: 'assign_datetime', render: (data) => data != null ? moment(data).format('YYYY-MM-DD / hh:mm A') : 'N/A'}
+                ],
+                dom: '<"row"<"col-12 col-md-6"l><"col-12 col-md-6"f>>' +
+                        '<"row"<"col-12"t>>' +
+                        '<"row"<"col-12 col-md-5"i><"col-12 col-md-7"p>>',
+                lengthMenu: [[5, 10, 50, 100, -1], [5, 10, 50, 100, "All"]],
+                searching: true,
+            });
+        });
+    </script>
 
     <script>
         const colors = {
@@ -345,7 +371,7 @@
         </div>
         <!-- End Quick Summary row -->
 
-        <div class="row">
+        <div class="row mb-4">
             <!-- Overall Tourists Graph -->
             @include('admin.layouts.dashboard.tourists-visited-graph')
             <!-- End Overall Tourists Graph -->
@@ -354,5 +380,32 @@
             @include('admin.layouts.dashboard.reminders')
             <!-- End Reminders Section -->
         </div> <!-- row -->
+
+        <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h6 class="card-title">Registered Tourists</h6>
+                        <p class="text-muted mb-3">List of registered tourists and their full information.</p>
+                        <div class="table-responsive">
+                            <table id="registeredTouristsTable" class="table">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Gender</th>
+                                        <th>Age</th>
+                                        <th>Cellphone No.</th>
+                                        <th>Date / Time</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
